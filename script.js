d70077037ea636ad32c5870a1b8c6ce5350b32f5
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-09-09 00:23:50 +0300

    Added styles to the form

--- script.js
+++
@@ -1,6 +0,0 @@
-const titleEl = document.querySelector(".title");
-const btnEl = document.querySelector(".btn");
-
-btnEl.addEventListener("click", () => {
-    titleEl.style.backgroundColor = "pink";
-})
