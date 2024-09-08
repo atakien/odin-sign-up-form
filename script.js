26a4f1179973a0f02c40eda6a28ff4ea06c05a0a
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-09-08 22:46:27 +0300

    Created files, html content and some styles
    Left side styles are added using flex and
    positioning.

---
+++ script.js
@@ -0,0 +1,6 @@
+const titleEl = document.querySelector(".title");
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", () => {
+    titleEl.style.backgroundColor = "pink";
+})
