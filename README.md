46d1c1a99d3acc3814368fe4706ee1b0e6db3444
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-09-09 14:12:17 +0300

    Added validation to the form inputs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-sign-up-form
+
+This is a sign up form page developed for ODIN project. 
+CSS features such as form validation, positioning and flexbox are used for styling.
---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+
+const btnEl = document.querySelector(".submit");
+            
+btnEl.addEventListener("click", function (event) {
+    event.preventDefault();
+ });
